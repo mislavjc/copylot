09b6476a1137e922d2604cfaad42ca68b6923ca9
Mislav <mislavjc@gmail.com>
2023-05-02 21:16:56 +0200

    feat: add geo util

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,15 @@
 import { ClassValue, clsx } from 'clsx';
 import { twMerge } from 'tailwind-merge';
 
-export function cn(...inputs: ClassValue[]) {
+export const cn = (...inputs: ClassValue[]) => {
   return twMerge(clsx(inputs));
-}
+};
+
+export const getGeoData = (request: Request) => {
+  return {
+    ip: request.headers.get('x-real-ip'),
+    region: request.headers.get('x-vercel-ip-country-region'),
+    country: request.headers.get('x-vercel-ip-country'),
+    city: request.headers.get('x-vercel-ip-city'),
+  };
+};
