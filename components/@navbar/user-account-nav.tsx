b0eda559066ccd38965497fd5d223a5296584f27
Mislav <mislavjc@gmail.com>
2023-05-15 22:30:44 +0200

    fix: guest user broken nav

--- components/@navbar/user-account-nav.tsx
+++ components/@navbar/user-account-nav.tsx
@@ -23,17 +23,17 @@ export function UserAccountNav({ user }: UserAccountNavProps) {
     <DropdownMenu>
       <DropdownMenuTrigger>
         <UserAvatar
-          user={{ name: user.name || null, image: user.image || null }}
+          user={{ name: user?.name || null, image: user?.image || null }}
           className="w-8 h-8"
         />
       </DropdownMenuTrigger>
       <DropdownMenuContent align="end">
         <div className="flex items-center justify-start gap-2 p-2">
           <div className="flex flex-col space-y-1 leading-none">
-            {user.name && <p className="font-medium">{user.name}</p>}
-            {user.email && (
+            {user?.name && <p className="font-medium">{user.name}</p>}
+            {user?.email && (
               <p className="w-[200px] truncate text-sm text-muted-foreground">
-                {user.email}
+                {user?.email}
               </p>
             )}
           </div>
