f07c730a920b0e25fe8de5d95c141c5d1ebbeff6
Mislav <mislavjc@gmail.com>
2023-05-16 20:56:51 +0200

    feat: add middleware for non org users

--- middleware.ts
+++ middleware.ts
@@ -1,7 +1,31 @@
-import middleware from 'next-auth/middleware';
+import withAuth from 'next-auth/middleware';
 
-export default middleware({
+export default withAuth({
   pages: {
     signIn: '/login',
   },
+  callbacks: {
+    authorized: ({ req, token }) => {
+      const isDashboardUrl = req.nextUrl.pathname.includes('/dashboard');
+
+      if (isDashboardUrl && !token?.organizationId) {
+        return false;
+      }
+
+      return true;
+    },
+  },
 });
+
+export const config = {
+  matcher: [
+    /*
+     * Match all request paths except for the ones starting with:
+     * - api (API routes)
+     * - _next/static (static files)
+     * - _next/image (image optimization files)
+     * - favicon.ico (favicon file)
+     */
+    '/((?!api|_next/static|_next/image|favicon.ico).*)',
+  ],
+};
