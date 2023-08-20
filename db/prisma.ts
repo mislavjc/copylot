61d1e5c7421e9d14462be996bc84c18eaafcc7fb
Mislav <mislavjc@gmail.com>
2023-08-21 00:49:06 +0200

    fix: prisma init code

--- db/prisma.ts
+++ db/prisma.ts
@@ -1,10 +1,11 @@
 import { PrismaClient } from '@prisma/client/edge';
 
-declare global {
-  // eslint-disable-next-line no-var
-  var prisma: PrismaClient | undefined;
-}
+const globalForPrisma = globalThis as unknown as {
+  prisma: PrismaClient | undefined;
+};
 
-const prisma = globalThis.prisma || new PrismaClient();
+const prisma = globalForPrisma.prisma ?? new PrismaClient();
+
+if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma;
 
 export default prisma;
