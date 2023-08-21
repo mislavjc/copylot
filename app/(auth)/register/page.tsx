cc070289ab4cd0b9aa278273cc5164c264f1a0da
Mislav <mislavjc@gmail.com>
2023-08-21 19:01:14 +0200

    fix: register page layout

--- app/(auth)/register/page.tsx
+++ app/(auth)/register/page.tsx
@@ -1,11 +1,7 @@
 import Link from 'next/link';
 
-import { buttonVariants } from 'ui/button';
-
 import { Icons } from 'components/icons';
 import { UserAuthForm } from 'components/user-auth-form';
-
-import { cn } from 'lib/utils';
 
 export const metadata = {
   title: 'Create an account',
@@ -14,16 +10,7 @@ export const metadata = {
 
 export default function RegisterPage() {
   return (
-    <div className="container grid h-screen w-screen flex-col items-center justify-center lg:max-w-none lg:grid-cols-2 lg:px-0">
-      <Link
-        href="/login"
-        className={cn(
-          buttonVariants({ variant: 'ghost' }),
-          'absolute right-4 top-4 md:right-8 md:top-8',
-        )}
-      >
-        Login
-      </Link>
+    <div className="container grid h-screen w-full flex-col items-center justify-center lg:max-w-none lg:grid-cols-2 lg:px-0">
       <div className="hidden h-full bg-muted lg:block" />
       <div className="lg:p-8">
         <div className="mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[350px]">
@@ -38,21 +25,10 @@ export default function RegisterPage() {
           </div>
           <UserAuthForm />
           <p className="px-8 text-center text-sm text-muted-foreground">
-            By clicking continue, you agree to our{' '}
-            <Link
-              href="/terms"
-              className="hover:text-brand underline underline-offset-4"
-            >
-              Terms of Service
-            </Link>{' '}
-            and{' '}
-            <Link
-              href="/privacy"
-              className="hover:text-brand underline underline-offset-4"
-            >
-              Privacy Policy
+            Already have an account?{' '}
+            <Link href="/login" className="underline">
+              Login.
             </Link>
-            .
           </p>
         </div>
       </div>
