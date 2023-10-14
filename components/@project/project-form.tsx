364d29a047e518ad579b12fe46c525b68c6f37c9
Mislav <mislavjc@gmail.com>
2023-10-14 16:30:22 +0200

    feat: make the URL need a specific format

--- components/@project/project-form.tsx
+++ components/@project/project-form.tsx
@@ -8,8 +8,17 @@ import { z } from 'zod';
 
 import { Button } from 'ui/button';
 import { Input } from 'ui/input';
-import { Label } from 'ui/label';
 import { toast } from 'ui/use-toast';
+
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from 'components/ui/form';
 
 import { createProject, updateProject } from 'lib/api/actions/projects';
 import { projectFormCreateSchema } from 'lib/validations/project';
@@ -21,7 +30,7 @@ interface ProjectFormProps {
 }
 
 export const ProjectForm = ({ project }: ProjectFormProps) => {
-  const { register, handleSubmit } = useForm<FormData>({
+  const form = useForm<FormData>({
     resolver: zodResolver(projectFormCreateSchema),
     defaultValues: {
       name: project?.name,
@@ -54,35 +63,47 @@ export const ProjectForm = ({ project }: ProjectFormProps) => {
 
   return (
     <div>
-      <form className="flex-col gap-4" onSubmit={handleSubmit(onSubmit)}>
-        <div className="mb-3 grid w-full items-center gap-1.5">
-          <Label htmlFor="name">Project name</Label>
-          <Input
-            type="text"
-            id="name"
-            placeholder="New Project"
-            {...register('name')}
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6">
+          <FormField
+            control={form.control}
+            name="name"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Project Name</FormLabel>
+                <FormControl>
+                  <Input placeholder="New Project" {...field} />
+                </FormControl>
+                <FormDescription>You can change this later.</FormDescription>
+                <FormMessage />
+              </FormItem>
+            )}
           />
-          <p className="text-sm text-muted-foreground">
-            You can change this later.
-          </p>
-        </div>
-        <div className="mb-3 grid w-full items-center gap-1.5">
-          <Label htmlFor="url">Website URL</Label>
-          <Input
-            type="text"
-            id="url"
-            placeholder="New Project"
-            {...register('url')}
+          <FormField
+            control={form.control}
+            name="url"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Website URL</FormLabel>
+                <FormControl>
+                  <Input placeholder="www.example.com" {...field} />
+                </FormControl>
+                <FormDescription>
+                  The URL of the website you want to track.
+                </FormDescription>
+                <FormMessage>
+                  {form.formState.errors.url && (
+                    <span className="text-red-500">
+                      {form.formState.errors.url.message}
+                    </span>
+                  )}
+                </FormMessage>
+              </FormItem>
+            )}
           />
-          <p className="text-sm text-muted-foreground">
-            The URL of the website you want to track.
-          </p>
-        </div>
-        <Button type="submit">
-          {project ? 'Update project' : 'Create project'}
-        </Button>
-      </form>
+          <Button type="submit">Submit</Button>
+        </form>
+      </Form>
     </div>
   );
 };
--- lib/validations/project.ts
+++ lib/validations/project.ts
@@ -1,6 +1,12 @@
 import { z } from 'zod';
 
+const urlRegex = /^(www|([a-z0-9]+(-[a-z0-9]+)*))\.[a-z]{2,}(\.[a-z]{2,})?$/;
+
+const urlValidation = z.string().refine((url) => urlRegex.test(url), {
+  message: 'URL must be in the format www.domain.com or subdomain.domain.com',
+});
+
 export const projectFormCreateSchema = z.object({
   name: z.string(),
-  url: z.string(),
+  url: urlValidation,
 });
