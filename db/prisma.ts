5e64c255b3c62c5269e96627c8bd8ca986cab6ce
Mislav <mislavjc@gmail.com>
2023-10-14 12:31:37 +0200

    feat: use new serverless adapter planetscale

--- db/prisma.ts
+++ db/prisma.ts
@@ -1,10 +1,21 @@
-import { PrismaClient } from '@prisma/client/edge';
+import { connect } from '@planetscale/database';
+import { PrismaPlanetScale } from '@prisma/adapter-planetscale';
+import { PrismaClient } from '@prisma/client';
+
+const connectionString = `${process.env.DATABASE_URL}`;
+
+const connection = connect({ url: connectionString });
+const adapter = new PrismaPlanetScale(connection);
 
 const globalForPrisma = globalThis as unknown as {
   prisma: PrismaClient | undefined;
 };
 
-const prisma = globalForPrisma.prisma ?? new PrismaClient();
+const prisma =
+  globalForPrisma.prisma ??
+  new PrismaClient({
+    adapter,
+  });
 
 if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma;
 
