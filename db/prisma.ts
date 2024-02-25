9e52a3c5d301ea95181e9e80116c7bb6dc7ebee8
Mislav <mislavjc@gmail.com>
2024-02-25 20:18:24 +0100

    fix: add missing semicolon

--- db/prisma.ts
+++ db/prisma.ts
@@ -4,7 +4,7 @@ import { PrismaClient } from '@prisma/client';
 
 const connectionString = `${process.env.DATABASE_URL}`;
 
-const client = new Client({ url: connectionString })
+const client = new Client({ url: connectionString });
 const adapter = new PrismaPlanetScale(client);
 
 const globalForPrisma = globalThis as unknown as {
